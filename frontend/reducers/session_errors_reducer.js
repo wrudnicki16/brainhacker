d3c8387f1a5442dd50a9de84a8bd09c02ac511b5
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2018-01-30 17:55:17 -0800

    Fix rootReducer state shape

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,12 +1,12 @@
 import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
 import { merge } from "lodash";
 
-const sessionErrorsReducer = (oldState = {}, action) => {
+const sessionErrorsReducer = (oldState = [], action) => {
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, oldState, {errors: []});
+      return merge({}, oldState, []);
     case RECEIVE_ERRORS:
-      return merge({}, oldState, {errors: action.errors});
+      return merge({}, oldState, action.errors);
     default:
       return oldState;
   }
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -2,7 +2,7 @@ import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { merge } from "lodash";
 
 
-const sessionReducer = (oldState = {currentUser: null, errors: []}, action) => {
+const sessionReducer = (oldState = {currentUser: null}, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
