1de002d88539f1c045c1520900595aa141e4cce0
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2018-02-06 20:06:40 -0800

    Make cards update properly

--- frontend/components/card/card_index_row_form.jsx
+++ frontend/components/card/card_index_row_form.jsx
@@ -4,10 +4,13 @@ import FontAwesome from 'react-fontawesome';
 
 class CardIndexRowForm extends React.Component {
   handleChange(e) {
+    const { id } = this.props.card;
     if (e.target.className === "row-front") {
       this.setState(merge({}, this.state, { card: { front: e.target.value}}));
+      this.props.updateCard({card: { id: id, front: e.target.value }});
     } else {
       this.setState(merge({}, this.state, { card: { back: e.target.value}}));
+      this.props.updateCard({card: { id: id, back: e.target.value }});
     }
   }
 
