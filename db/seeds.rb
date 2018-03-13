9c6801f33c0badc5da625f086503b7f8088c6f4a
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2018-03-12 23:25:29 -0700

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,24 @@
 users = User.create([
   {username: "Demo", password: "password"},
   {username: "wyattrud", password: "password"}
-]);
+])
 
 decks = Deck.create([
   { title: "Psychology", creator_id: 1 },
   { title: "Sleep", creator_id: 2 },
 
-]);
+])
 
 cards = Card.create([
   { front: "Function of forgetting", back: "e^-x", deck_id: 1 },
 
 ##### sleep ######
   { front: "What is the average half-life of caffeine?", back: "5-7 hours", deck_id: 2 },
+  { front: "How long are people typically awake for, given sunlight cues?", back: "15-16 hours", deck_id: 2 },
+  { front: "What constitutes the advent of natural waking, besides sunlight?", back: "Reduced adenosine and circadian rhythm", deck_id: 2 },
+  { front: "Does circadian rhythm get affected by a lack of sleep?", back: "No, it continues to follow a stable pattern, which gives a second wind in the morning, followed by a crushing urge to sleep around midday.", deck_id: 2 },
+  { front: "What 2 questions indicate whether you need higher quantity/quality of sleep?",
+    back: "1. After waking in the morning, could you fall back to sleep at ten or eleven am? \n
+    2. Can you function optimally without caffeine before noon?", deck_id: 2 },
   { front: "", back: "", deck_id: 2 },
-  ])
+])
