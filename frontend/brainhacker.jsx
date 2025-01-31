ca2e7ba8bfa06bf40378413924057985340a1ec4
wrudnicki16 <wyattrudnicki@gmail.com>
2025-01-31 15:22:14 -0800

    Readd application file

--- frontend/brainhacker.jsx
+++ frontend/brainhacker.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { searchDecks } from './actions/search_deck_actions';
 import Root from './components/root.jsx';
-import Rails from '@rails/ujs';
-import $ from 'jquery';
-
-Rails.start();
-window.$ = $; // Make jQuery globally available if needed
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
