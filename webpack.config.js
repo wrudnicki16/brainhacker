516878c00710f500aab40d2ffb08fd3de13353dd
wrudnicki16 <wyattrudnicki@gmail.com>
2025-01-31 14:49:09 -0800

    Moving bundle to separate folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/brainhacker.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, 'app', 'assets', 'builds'),
     filename: "bundle.js"
   },
   module: {
